093df4068ecb9faf8550e468daeeee02c6037c04
akram <akramibnelyazid@gmail.com>
2024-12-29 18:41:36 +0100

    add form cabins

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   useEffect(function () {
     getCabins().then((data) => console.log(data));
   }, []);
@@ -16,6 +19,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          {showForm ? "Hide form" : "Add cabin"}
+        </Button>
+        {showForm && <CreateCabinForm/>}
       </Row>
     </>
   );
